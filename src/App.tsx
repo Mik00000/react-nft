99993a1e1ee31dbdd1a9b0245598e1a840bc03dd
display-inline-flex <mikjarkov2@gmail.com>
2025-02-25 16:36:29 +0200

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom"
+
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Routes>
+
+      </Routes>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/sections/Header.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
+const Header = () => {
+  const headerMenu = [
+    {
+      name: "Explore",
+      link: "/explore",
+    },
+    {
+      name: "Creators",
+      link: "/creators",
+    },
+    {
+      name: "Community",
+      link: "/community",
+    },
+  ];
+  return (
+    <header className="header">
+      <img src={logo} alt="logo" className="logo" />
+      <nav className="header-menu">
+        {headerMenu.map((item)=>(
+            <Link to={item.link}>{item.name}</Link>
+        ))}
+      </nav>
+      <Button bgColor="#E9D7A7" bgOpacity={0.1} textColor="#FFCE4E" padding="21px 30px" borderColor="#FFCE4E">Connect Wallet</Button>
+    </header>
+
+  );
+};
+
+export default Header;
