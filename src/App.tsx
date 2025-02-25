1901c4ccb8ed6abe6a358a0022aca04f476ee15b
display-inline-flex <mikjarkov2@gmail.com>
2025-02-25 17:06:02 +0200

    Header Styles v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./styles/App.css";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
-} from "react-router-dom"
-
+} from "react-router-dom";
+import Header from "./sections/Header";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Routes>
-
-      </Routes>
-    </div>
+      <div className="App">
+        <Header />
+        <Routes></Routes>
+      </div>
     </Router>
   );
 }
