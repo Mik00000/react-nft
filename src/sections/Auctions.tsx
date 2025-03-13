6156966d14c970400397e55c498d29d164f9d495
display-inline-flex <mikjarkov2@gmail.com>
2025-03-13 17:50:43 +0200

    Feature: Auctions(not so raw)

--- src/sections/Auctions.tsx
+++ src/sections/Auctions.tsx
@@ -9,7 +9,8 @@ import auction1 from "../assets/images/cards/auction-1.png";
 import auction2 from "../assets/images/cards/auction-2.png";
 import auction3 from "../assets/images/cards/auction-3.png";
 
-import likeIcon from "../assets/images/icons/like.svg"
+import likeIcon from "../assets/images/icons/like.svg";
+import Button from "../components/Button";
 const cards = [
   {
     img: auction1,
@@ -41,12 +42,12 @@ const cards = [
 ];
 const Auctions = () => {
   return (
-    <div>
+    <div className="auctions">
       <Heading bubleColor="#8613A5">Live Auctions</Heading>
 
       <Carousel
         slideSize="70%"
-        height={200}
+        height={691}
         align="start"
         slideGap="md"
         controlsOffset="xs"
@@ -56,22 +57,52 @@ const Auctions = () => {
         {cards.map((card) => (
           <Carousel.Slide>
             <div className="card">
-              <div className="image" style={{
-                backgroundImage:`url(${card.img})`
-              }}>
+              <div
+                className="image"
+                style={{
+                  backgroundImage: `url(${card.img})`,
+                }}
+              >
                 <span className="timer">{card.time}</span>
               </div>
               <div className="title">
                 <div className="author">
-                    <span className="author-name">{card.authorName}</span>
-                    <span className="author-tag">by {card.authorTag}</span>
-                </div>
-                <div className="likes">
-                
+                  <div className="left-part">
+                    <div className="author-icon">
+                      <img src={card.authorImage} alt="author-icon" />
+                    </div>
+                    <div className="author-info">
+                      <span className="author-name">{card.authorName}</span>
+                      <span className="author-tag">by {card.authorTag}</span>
+                    </div>
+                  </div>
+                  <div className="right-part">
+                    <div className="likes">
+                      <img
+                        src={likeIcon}
+                        alt="like-icon"
+                        className="like-icon"
+                      />
+                      <span>{card.likes}</span>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="price"></div>
-              <div className="place-bid"></div>
+              <div className="price">
+                <span>Current Bid</span>
+                <span className="price-value">{card.price}</span>
+              </div>
+              <div className="place-bid">
+                <Button
+                  padding="21px 145px"
+                  textColor="#fff"
+                  bgColor="#e9d7a7"
+                  bgOpacity={0.1}
+                  borderColor="#fde8b3"
+                >
+                  Place a bid
+                </Button>
+              </div>
             </div>
           </Carousel.Slide>
         ))}
